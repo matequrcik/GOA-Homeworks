cd8c5089a7aee28a38a9014af5aebbe67b6ce325
mate <matequrcik@gmail.com>
2024-02-19 21:31:16 +0400

    git

---
+++ day 05/homework 05.py
@@ -0,0 +1,8 @@
+mate = name"  #სწორია name = (mate) მაშინ ბრჭყალები არ უნდა გამოიყენო. დაპრინტავს
+print("mate) #მაშინ ბრჭყალები არ უნდა
+ print(3666+35355f) #რიცხვებთან ასოს გამოყენება არ შეგვიძლია
+age-> 5356 # -> age თან არ უნდა იყოს
+print(bassket)  #უნდა იყოს ბრჭყალებში
+
+print(gpgk)
+gpgk = 5623  #unda iyos piriqit mashin dawers erors
